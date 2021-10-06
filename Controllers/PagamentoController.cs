1768dd93033b624f4d49aa82b7392874bf33f57b
kaiovv <89214412+kaiovv@users.noreply.github.com>
2021-10-06 16:56:46 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/PagamentoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pagamento.Controllers
+{
+    public class PagamentoController
+    {
+       
+        public string TipoPagamento { get; set; }
+    }   
+        
+}
